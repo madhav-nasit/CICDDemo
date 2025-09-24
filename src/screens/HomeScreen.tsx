aff22195ec0b2f15095d768174fa859631e7f173
Madhav Nasit <madhavn@mindfiresolutions.com>
2025-09-24 15:16:35 +0530

    feat: introduce issues

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -25,8 +25,8 @@ const HomeScreen: React.FC = () => {
   const dispatch = useAppDispatch();
 
   // ISSUE: Eslint & Typescript
-  // const handleDeleteTodo = id => {
-  const handleDeleteTodo = (id: string) => {
+  const handleDeleteTodo = id => {
+    // const handleDeleteTodo = (id: string) => {
     dispatch(removeTodo(id));
   };
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 // ISSUE: Introduce test error
 export const addTwoDigits = (a: number, b: number): number => {
-  return a + b;
-  // return a - b;
+  // return a + b;
+  return a - b;
 };
