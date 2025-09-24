6f58bc46386de517dcab32cd7737ab5e6ce0b21c
Madhav Nasit <madhavn@mindfiresolutions.com>
2025-09-24 11:33:01 +0530

    fix: Resolve typescript issue

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -20,11 +20,10 @@ const TodoCard: React.FC<TodoCardProps> = ({
   completed,
   onDelete,
   onToggle,
-  dummyProp,
 }) => (
   <View key={id} style={[styles.card, completed && styles.completedCard]}>
     <TouchableOpacity style={styles.toggleButton} onPress={onToggle}>
-      <View style={[styles.checkbox, completed && styles.checkedBox]}>
+        <View style={[styles.checkbox, completed && styles.checkedBox]}>
         {completed && <Text style={styles.checkmark}>✓</Text>}
       </View>
     </TouchableOpacity>
